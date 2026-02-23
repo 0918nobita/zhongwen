2ffdfad54ffd9b39a7a99b386849e6a96e6d526b
0918nobita <nobita.0918@gmail.com>
2026-02-23 19:06:59 +0900

    因引入 oxfmt 调整 oxlint、eslint 配置

--- client/eslint.config.ts
+++ client/eslint.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import oxlint from 'eslint-plugin-oxlint';
-import ts from 'typescript-eslint';
 import vue from 'eslint-plugin-vue';
+import ts from 'typescript-eslint';
 
 export default defineConfig(
   {
-    ignores: ['src-tauri'],
+    ignores: [
+      'dist',
+      'node_modules',
+      'src-tauri',
+    ],
   },
   js.configs.recommended,
   ts.configs.recommendedTypeChecked,
@@ -27,6 +31,20 @@ export default defineConfig(
         parser: ts.parser,
         extraFileExtensions: ['.vue'],
       },
+    },
+    rules: {
+      'vue/html-self-closing': ['error', {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+      }],
+      'vue/max-attributes-per-line': ['error', {
+        singleline: 5,
+        multiline: 1,
+      }],
+      'vue/singleline-html-element-content-newline': 'off',
     },
   },
   ...oxlint.configs['flat/all'],
