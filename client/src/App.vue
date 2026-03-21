2caaaa2416b2a5be50834f3d72a9cb5a5df347cd
0918nobita <nobita.0918@gmail.com>
2026-03-21 12:31:21 +0900

    添加并配置 Noto Sans CJK SC 字体

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { invoke } from '@tauri-apps/api/core';
+import { html } from 'pinyin-pro';
 import * as v from 'valibot';
 import { onMounted, ref } from 'vue';
 
@@ -7,7 +8,11 @@ const GreetMsg = v.tuple([v.string(), v.string()]);
 
 const lines = ref<string[]>([]);
 
+const pinyinHtmlString = ref<string>('');
+
 onMounted(async () => {
+  pinyinHtmlString.value = html('病从口入，祸从口出。');
+
   try {
     const res = await invoke('greet', { name: 'Kodai' });
     const parsed = v.parse(GreetMsg, res);
@@ -25,4 +30,5 @@ onMounted(async () => {
       {{ line }}
     </template>
   </p>
+  <div v-html="pinyinHtmlString" />
 </template>
