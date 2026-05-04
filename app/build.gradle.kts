34cb0ead14a1bcbc16f8cbea0eb176e7110e343c
0918nobita <nobita.0918@gmail.com>
2026-05-04 15:13:45 +0900

    Kotlin Logging を用いて、レスポンスのJSON文字列をログ出力する

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.ktor.client.okhttp)
     implementation(libs.ktor.client.logging)
     implementation(libs.slf4j.api)
+    implementation(libs.kotlin.logging.jvm)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/engineer/kodai/yanniang/PostViewModel.kt
+++ app/src/main/java/engineer/kodai/yanniang/PostViewModel.kt
@@ -2,14 +2,19 @@ package engineer.kodai.yanniang
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+
+private val logger = KotlinLogging.logger {}
 
 class PostViewModel : ViewModel() {
     fun fetchPost() {
         viewModelScope.launch(Dispatchers.IO) {
-            httpClient.get("https://jsonplaceholder.typicode.com/posts/1")
+            val text = httpClient.get("https://jsonplaceholder.typicode.com/posts/1").bodyAsText()
+            logger.info { text }
         }
     }
 }
