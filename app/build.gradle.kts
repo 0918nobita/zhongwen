b453f38daed440375f59b57264ba3935647df72e
0918nobita <nobita.0918@gmail.com>
2026-05-04 11:11:35 +0900

    依存ライブラリとして ktor-client-okhttp を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
